0af44b50b8b85dfa6bb5d78aecf234bf007750d2
ometman <revomet@gmail.com>
2023-05-10 16:45:44 +0000

    Setup webpack with basic files

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import _ from 'lodash';
+import printMe from './print.js';
+import './style.css';
+
+
+function component() {
+    const element = document.createElement('div');
+    const btn = document.createElement('button');
+    const brkLine = document.createElement('br');
+
+    // Lodash, is imported
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    element.appendChild(brkLine)
+    
+    btn.innerHTML = 'Click me and check the console!';
+
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
+    return element;
+}
+
+document.body.appendChild(component());
